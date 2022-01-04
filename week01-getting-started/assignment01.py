483864b561a4c09f86da1ec0f1389b0348be466d
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-01-04 09:48:50 -0300

    Week 01 activities added

---
+++ week01-getting-started/assignment01.py
@@ -0,0 +1,5 @@
+name = input('Welcome, please enter your name ')
+color = input('Now, type your favorite color')
+print('Thank you for entering that information! \nLOADING......')
+
+print(f"Hello, {name}, nice! Your favorite color is {color}")
