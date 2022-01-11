4cd540772074daca9812fc0588a9e19ad11d8f39
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-01-11 10:42:33 -0300

    update assignment 02

--- week02_input_output/assignment02.py
+++ week02_input_output/assignment02.py
@@ -1,19 +1,24 @@
 #02 Prove: Assignment - Word Games
 #Student: Felipe dos Santos Belisario
 
-print('Please enter the following')
+print('Please enter the following:')
 print()
 adjective = input('Adjective: ')
-animal = input('Animal: ')
-first_verb = input('Verb: ')
-exclamation = input('Word for exclamation: ')
-second_verb = input('Different Verb: ')
-third_verb = input('Another Verb: ')
+plural_noun = input('Plural Noun: ')
+noun = input('Noun: ')
+color = input('Color: ')
+first_adverb = input('Adverb: ')
+number = input('Number: ')
+past_verb = input('Past Tense Verb: ')
+est_adjective = input('-est Adjective: ')
+second_past_verb = input('Past Tense Verb: ')
+second_adverb = input('Adverb: ')
+third_adjective = input('Adjective: ')
 print()
 print('Your story is:')
 print()
-print(f"The other day, I was really in trouble. It all started when I saw a very {adjective.upper()} {animal.upper()} {first_verb.upper()} down the hallway. \"{exclamation.upper()}!\" I yelled. But all I could think to do was to {second_verb.upper()} over and over. Miraculously, that caused it to stop, but not before it tried to {third_verb.upper()} right in front of my family.")
+print('The Fun Park')
+print()
+print(f"Today, my fabulous camp group went to a (an) {adjective.upper()} amusement park. It was a fun park with lots of cool  {plural_noun.upper()} and enjoyable play structures. When we got there, my kind counselor shouted loudly, \"Everybody off the {noun.upper()}.\" We all pushed out in a terrible hurry. My counselor handed out {color.upper()} tickets, and we scurried in. I was so excited! I couldn't figure out what exciting thing to do first. I saw a scary roller coaster I really liked so, I  {first_adverb.upper()} ran over to get in the long line that had about {number.upper()} people in it. When I finally got on the roller coaster I was  {past_verb.upper()}. In fact I was so nervous my two knees were knocking together. This was the  {est_adjective.upper()} ride I had ever been on! In about two minutes I heard the crank and grinding of the gears. That’s when the ride began! When I got to the bottom, I was a little  {second_past_verb.upper()} but I was proud of myself. The rest of the day went {second_adverb.upper()}. It was a (an) {third_adjective.upper()} day at the fun park.")
 
 
-#PLEASE BEFOR SENDING THIS ASSIGNMENT, CHECK IF ALREADY DO EVEYTHING
-# NEED TO INSERT => Showing Creativity and Exceeding Requirements
