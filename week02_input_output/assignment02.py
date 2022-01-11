8810ed978069c249344b8f1b10351f2c7d8763a2
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-01-10 21:37:46 -0300

    add images

--- week02_input_output/assignment02.py
+++ week02_input_output/assignment02.py
@@ -13,3 +13,7 @@ print()
 print('Your story is:')
 print()
 print(f"The other day, I was really in trouble. It all started when I saw a very {adjective.upper()} {animal.upper()} {first_verb.upper()} down the hallway. \"{exclamation.upper()}!\" I yelled. But all I could think to do was to {second_verb.upper()} over and over. Miraculously, that caused it to stop, but not before it tried to {third_verb.upper()} right in front of my family.")
+
+
+#PLEASE BEFOR SENDING THIS ASSIGNMENT, CHECK IF ALREADY DO EVEYTHING
+# NEED TO INSERT => Showing Creativity and Exceeding Requirements
