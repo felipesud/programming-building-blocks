8172c7c6c866ea65a21f845cfbd90eb879f131a0
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-01-10 21:16:49 -0300

    assgnment 02 added

---
+++ week02_input_output/assignment02.py
@@ -0,0 +1,15 @@
+#02 Prove: Assignment - Word Games
+#Student: Felipe dos Santos Belisario
+
+print('Please enter the following')
+print()
+adjective = input('Adjective: ')
+animal = input('Animal: ')
+first_verb = input('Verb: ')
+exclamation = input('Word for exclamation: ')
+second_verb = input('Different Verb: ')
+third_verb = input('Another Verb: ')
+print()
+print('Your story is:')
+print()
+print(f"The other day, I was really in trouble. It all started when I saw a very {adjective.upper()} {animal.upper()} {first_verb.upper()} down the hallway. \"{exclamation.upper()}!\" I yelled. But all I could think to do was to {second_verb.upper()} over and over. Miraculously, that caused it to stop, but not before it tried to {third_verb.upper()} right in front of my family.")
