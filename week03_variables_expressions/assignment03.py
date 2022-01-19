498ae5d76fde9e9ff3d25c63761058d0df1d77cd
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-01-19 15:28:43 -0300

    Update assignment03.py

--- week03_variables_expressions/assignment03.py
+++ week03_variables_expressions/assignment03.py
@@ -3,6 +3,11 @@ a_meal = input('What is the price of an adult\'s meal?')
 children = input('How many children are there?')
 adults = input('How many adults are there?')
 tax_rate = input('What is the sales tax rate?')
+print(f"What is the price of a child\'s meal? ${c_meal}")
+print(f"What is the price of an adult\'s meal? ${a_meal}")
+print(f"How many children are there? {children}")
+print(f"How many adults are there? {adults}")
+print(f"What is the sales tax rate? {tax_rate}")
 subtotal = 0
 sales_tax = 0
 total = 0
@@ -12,3 +17,4 @@ print(f"Total: {total}")
 
 payment = input('\nWhat is the payment amount?')
 change = float(payment) - float(total)
+print(f"Change: ${change}")
