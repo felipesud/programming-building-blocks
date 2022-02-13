d413a29b2f48c308e4e7d964d645e461506a2927
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-02-12 23:53:03 -0300

    Update assignment06.py

--- week06_solving_problems_decisions/assignment06.py
+++ week06_solving_problems_decisions/assignment06.py
@@ -40,7 +40,7 @@ while True:
         if user_choice_way == 'save':
             user_choice_way2 = input('\nYou are a negotiator! Laban got interested and made a low offer, you now have a chance to RAISE the offer or FOLD. What do you want to do?').lower()
             if user_choice_way2 == 'raise':
-                print('Great! Laban accepted the offer and handed over the plates, now you can return to your father\'s tent ')
+                print('\nGreat! Laban accepted the offer and handed over the plates, now you can return to your father\'s tent ')
             elif user_choice_way2 == 'fold':
                 print('\nIn such a negotiation you cannot be afraid. Laban sensed your fear and ordered his guards to steal all of your treasure. Too bad, you lost your money and ran out of plates. Game Over')        
             break
@@ -50,30 +50,50 @@ while True:
     #3rd way
     if user_choice == 'disguise':
         user_choice_way2 = input('\nCool, you took advantage of Laban\'s absence and got dressed. The problem is that he will arrive soon, you have to be fast. You can RUN and do everything in a hurry, you can also impersonate Laban and SEND the servants to get the plates for you, or you can ENJOY and get Laban\'s sword for you before taking the plates.').lower()
+        
         if user_choice_way2 == 'run':
-            print('Well done! You got the plates in time before Laban returned. Congratulations, now you can go back to your father\'s tent. ')
-            break    
-        elif user_choice_way2 == 'send':
-            user_choice_way3 = input('\nFor the servant not to distrust you, take the SWORD or IMITATE the voice of Laban. Please choose a way forward').lower()
-            if user_choice_way3 == 'sword':
-                print('Zoran the servant is scared. You reassured him, you took the signs, and now you and he will return to your father\'s tent.')
-                break
-            elif user_choice_way3 == 'imitate':
-                user_choice_way4 = input('\nWhen ordering the servants to take the plates, you can either be SEVERE in your words or POLITE. How do you want to speak?').lower()
-                if user_choice_way4 == 'severe':
-                    print('\nVery well, this is how Laban treats his servants, no one suspected you, the servants took the plates, gave them to you. Now you can go back to your father\'s tent.')
+                print('\nWell done! You got the plates in time before Laban returned. Congratulations, now you can go back to your father\'s tent. ')
+                break    
+        if user_choice_way2 == 'send':
+                user_choice_way3 = input('\nFor the servant not to distrust you, take the SWORD or IMITATE the voice of Laban. Please choose a way forward').lower()
+            
+                if user_choice_way3 == 'sword':
+                    print('\nZoran the servant is scared. You reassured him, you took the plates, and now you and he will return to your father\'s tent.')
                     break
-                elif user_choice_way4 == 'polite':
-                    user_choice_way5 = input('\nThe servants noticed something strange, as Laban is not usually so polite. They saw you\'re an imposter and they want to arrest you. You can ESCAPE them or HIDE yourself to try and get the plates somehow.').lower()
-                    if user_choice_way5 == 'escape':
-                        print('\nUnfortunately, your cover didn\'t work and you didn\'t manage to get the plates. Try again.')
+                if user_choice_way3 == 'imitate':
+                    user_choice_way4 = input('\nWhen ordering the servants to take the plates, you can either be SEVERE in your words or POLITE. How do you want to speak?').lower()
+                    if user_choice_way4 == 'severe':
+                        print('\nVery well, this is how Laban treats his servants, no one suspected you, the servants took the plates, gave them to you. Now you can go back to your father\'s tent.')
                         break
-                    elif user_choice_way5 == 'hide':
-                        user_choice_way6 = input('\nNow you are hidden. Laban seems to be getting home, you need to be quick. Wait for Laban to arrive, you take the plates and sneak out the WINDOW, or you can grab the SWORD and fight the guards. What do you choose?').lower()
-                        if user_choice_way6 == 'window':
-                            print('\nCongratulations on your courage, you were smart and took the plates even though you were in an adverse situation.')
+                    if user_choice_way4 == 'polite':
+                        user_choice_way5 = input('\nThe servants noticed something strange, as Laban is not usually so polite. They saw you\'re an imposter and they want to arrest you. You can ESCAPE them or HIDE yourself to try and get the plates somehow.').lower()
+                        if user_choice_way5 == 'escape':
+                            print('\nUnfortunately, your cover didn\'t work and you didn\'t manage to get the plates. Try again.')
                             break
-                        elif user_choice_way6 == 'sword':
-                            print('\nAlone against all those guards, it would be impossible to win, wouldn\'t it? You were caught by the guards. Game Over')
-                            break
+                        if user_choice_way5 == 'hide':
+                            user_choice_way6 = input('\nNow you are hidden. Laban seems to be getting home, you need to be quick. Wait for Laban to arrive, you take the plates and sneak out the WINDOW, or you can grab the sword and FIGHT the guards. What do you choose?').lower()
+                            if user_choice_way6 == 'window':
+                                print('\nCongratulations on your courage, you were smart and took the plates even though you were in an adverse situation.')
+                                break
+                            if user_choice_way6 == 'fight':
+                                print('\nAlone against all those guards, it would be impossible to win, wouldn\'t it? You were caught by the guards. Game Over')
+                                break
+                            else:
+                                print("\nLet's try again, please, type: WINDOW or FIGHT\n")
+                        else:
+                            print("\nLet's try again, please, type: ESCAPE or HIDE\n")
+                    else:
+                        print("\nLet's try again, please, type: SEVERE or POLITE\n")
+                else:
+                    print("\nLet's try again, please, type: SWORD or IMITATE\n")
+            
+            #4th way
+                if user_choice_way2 == 'enjoy':
+                    print('You wasted a lot of time and Laban came home, the guards arrested you. Game over')
+                    break
+    else:
+        print("\nLet's try again, please, type: RUN, SEND or ENJOY\n")
+
+
+
 
