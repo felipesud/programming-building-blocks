d0d7533a4bd1ec9b1a801efe100e9f0e52f6f73b
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-02-18 22:11:37 -0300

    Update checkpoint07.py

---
+++ week07_loops/checkpoint07.py
@@ -0,0 +1,20 @@
+# Checkpoint Week 07: Demonstrate your understanding of loops 
+#From: https://byui-cse.github.io/cse110-course/lesson07/checkpoint.html
+#by: Felipe dos Santos Belisário
+
+#01 - Use a while loop to ask the user for a positive number (>= 0). Continue asking as long as the number is negative, then display the number.
+positive_number = 0
+is_positive = False
+while is_positive == False:
+    positive_number = float(input('Please type a positive number: '))
+    if positive_number >= 0:
+        print(f'The number is: {positive_number}')
+        is_positive = True
+    else:
+        print('Sorry, that is a negative number. Please try again. ')
+
+
+
+
+
+#02 - Use a while loop, to simulate a child asking their parent for a piece of candy. Have the program keep looping until the user answers "yes", then have the program output "Thank you."
