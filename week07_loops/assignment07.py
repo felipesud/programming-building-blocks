3bcdb2dc01e54d1b8579004c0abd911c64af523a
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-02-19 21:17:05 -0300

    Update images

---
+++ week07_loops/assignment07.py
@@ -0,0 +1,10 @@
+#Assignment Week 07 - Milestone
+#From: https://byui-cse.github.io/cse110-course/lesson07/prove.html
+#By: Felipe dos Santos Belisário
+
+animal = "rabbit"
+while animal == "dog":
+   print("a")
+   animal = "cat"
+   print("b")
+print("c")
