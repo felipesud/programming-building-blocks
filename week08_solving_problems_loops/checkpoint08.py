6608c3957296feea15ae59bd916d8f41f9d4d4a5
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-02-23 15:20:36 -0300

    Add checkpoint08.py

---
+++ week08_solving_problems_loops/checkpoint08.py
@@ -0,0 +1,7 @@
+#Checkpoint week 08 - Solving Problems Using Loops
+#From: https://byui-cse.github.io/cse110-course/lesson08/checkpoint.html
+#By: Felipe dos Santos Belisário
+
+colors = ["red", "blue", "green", "yellow"]
+for color in colors:
+    print(color)
