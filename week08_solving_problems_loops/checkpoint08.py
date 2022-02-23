1e1b8b8dfc6ff1275a115feab2f8208e63983b62
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-02-23 15:26:46 -0300

    Update checkpoint08.py

--- week08_solving_problems_loops/checkpoint08.py
+++ week08_solving_problems_loops/checkpoint08.py
@@ -2,6 +2,22 @@
 #From: https://byui-cse.github.io/cse110-course/lesson08/checkpoint.html
 #By: Felipe dos Santos Belisário
 
+#01 - Use a for loop to iterate through this list one by one and display each item on its own line
+print('Use a for loop to iterate through this list one by one and display each item on its own line')
 colors = ["red", "blue", "green", "yellow"]
 for color in colors:
     print(color)
+   
+
+
+#Use a for loop to display the numbers 1–8, one number on each line
+print('\nUse a for loop to display the numbers 1–8, one number on each line')
+for numbers in range(9):
+    print(numbers)
+
+
+
+#Use a for loop to display the even numbers (hint: count by two) 2–20, one number on each line 
+print('\nUse a for loop to display the even numbers (hint: count by two) 2–20, one number on each line ')
+for even_numbers in range(2, 22, 2):
+    print(even_numbers)
