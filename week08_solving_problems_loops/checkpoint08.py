3016c0cbef8492fb277fed1d9fafb30be17a095b
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-02-23 15:27:46 -0300

    Update checkpoint08.py

--- week08_solving_problems_loops/checkpoint08.py
+++ week08_solving_problems_loops/checkpoint08.py
@@ -3,21 +3,23 @@
 #By: Felipe dos Santos Belisário
 
 #01 - Use a for loop to iterate through this list one by one and display each item on its own line
-print('Use a for loop to iterate through this list one by one and display each item on its own line')
+print('01 - Use a for loop to iterate through this list one by one and display each item on its own line')
 colors = ["red", "blue", "green", "yellow"]
 for color in colors:
     print(color)
    
 
 
-#Use a for loop to display the numbers 1–8, one number on each line
-print('\nUse a for loop to display the numbers 1–8, one number on each line')
+#02 - Use a for loop to display the numbers 1–8, one number on each line
+print('\n02 - Use a for loop to display the numbers 1–8, one number on each line')
 for numbers in range(9):
     print(numbers)
 
 
 
-#Use a for loop to display the even numbers (hint: count by two) 2–20, one number on each line 
-print('\nUse a for loop to display the even numbers (hint: count by two) 2–20, one number on each line ')
+#03 - Use a for loop to display the even numbers (hint: count by two) 2–20, one number on each line 
+print('\n03 - Use a for loop to display the even numbers (hint: count by two) 2–20, one number on each line ')
 for even_numbers in range(2, 22, 2):
     print(even_numbers)
+
+
