aabfa6e7e89d7ef9066153e32afc49b64ba81caf
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-02-23 15:31:42 -0300

    Update checkpoint08.py

--- week08_solving_problems_loops/checkpoint08.py
+++ week08_solving_problems_loops/checkpoint08.py
@@ -18,7 +18,7 @@ for numbers in range(1, 9):
 
 
 #03 - Use a for loop to display the even numbers (hint: count by two) 2–20, one number on each line 
-print('\n03 - Use a for loop to display the even numbers (hint: count by two) 2–20, one number on each line ')
+print('\n03 - Use a for loop to display the even numbers (hint: count by two) 2–20, one number on each line')
 for even_numbers in range(2, 22, 2):
     print(even_numbers)
 
