886b831dd4ef505c46319517ed5fe43da842302b
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-03-09 19:16:54 -0300

    Update assignment10.py

--- week10_solving_problems_lists/assignment10.py
+++ week10_solving_problems_lists/assignment10.py
@@ -12,7 +12,7 @@ item_amount = 0
 to_remove = 0
 
 print('\n=================================================')
-print('*                 SHOPPING CART                 *')
+print('*                THE SHOPPING CART                 *')
 print('=================================================\n')
 
 #A menu system that repeats until the user chooses quit.
@@ -45,22 +45,25 @@ while True:
             print(f'{i + 1}.{item} - {amount} units for ${price:.2f} - Total: ${amount * price:.2f}')
      #The system will request wich item to remove, the user type the number. To find the right index, i did user_choice -1 (line 48)      
     elif new_item == 3:
-        
-        if len(items_cart) >= to_remove:
-                to_remove = int(input('Which item would you like to remove? ')) - 1
-        #With variable which_item I can show the name of item that were deleted, see on line 54
+         to_remove = int(input('Which item would you like to remove? ')) - 1
+         while len(items_cart) >= to_remove:
+                
+                #With variable which_item I can show the name of item that were deleted, see on line 54
                 which_item = items_cart[to_remove]
                 items_cart.pop(to_remove)
                 prices_cart.pop(to_remove)
                 items_quantities.pop(to_remove)
                 #print the result of operation to the user
                 print(f'{which_item} removed successfully.')
-                
-        else:
+                break  
+         else:
                 print('Please, enter a correct number')
     elif new_item == 4:
         #In this case I just did total of prices times total of amount to have the total of cost.
-        sum_prices = sum(prices_cart) * sum(items_quantities)
+        for i in range(len(items_quantities)):
+            price = prices_cart[i]
+            amount = items_quantities[i]
+        sum_prices = sum(amount * price)
         print(f'The total cost of the items in the shopping cart is: {sum_prices:.2f}')
     elif new_item == 5:
         print('\nThank you Goodbye')
