3f1bbc3587670e7504e14e440e1d74760b2d9818
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-03-19 22:26:27 -0300

    Update assignment11.py

--- week11_files/assignment11.py
+++ week11_files/assignment11.py
@@ -2,7 +2,18 @@
 # From: https://byui-cse.github.io/cse110-course/lesson11/prove.html
 # By: Felipe dos Santos Belisário
 
+from itertools import count
+
+
+
+
 with open('./life-expectancy.csv') as life_expectancy:
     for line in life_expectancy:
-        countries_data = line.split(",")
-        print(countries_data)
+        line = line.strip()
+        country, acronym, year, countries_data = line.split(",")
+      
+
+
+    expectancy_value = countries_data[3]
+    print(f"The max life expectancy was {max(expectancy_value)}")
+    print(f"The min life expectancy was {min(expectancy_value)}")
