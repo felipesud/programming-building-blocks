1099ed5e0e177ee479394ad1809ae5deafc45206
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-04-01 18:48:21 -0300

    Update assignment13.py

---
+++ assignment13.py
@@ -0,0 +1,24 @@
+#Assignment Week 13
+#From: https://byui-cse.github.io/cse110-course/lesson13/prove.html
+#By: Felipe dos Santos Belisário
+
+
+# Write a function to calculate and return the wind chill based on a given temperature and wind speed.
+def calculate_windchill(temperature, wind_speed):
+    windchill= 35.74 + (0.6215 * temperature) - 35.75 * (wind_speed ** 0.16 ) + (0.4275 * temperature) * (wind_speed ** 0.16 )
+    return windchill
+
+def celsius_to_fahrenheit(degree):
+    return (degree * (9 / 5) + 32)
+    
+
+user_temperature = float(input('What is the temperature? '))
+
+degree_type = input('Fahrenheit or Celsius (F/C)? ').upper()
+if user_temperature == 'C':
+    user_temperature = celsius_to_fahrenheit(user_temperature)
+        
+
+for wind_speed in range(5, 61, 5):
+    windchill = calculate_windchill(user_temperature, wind_speed)
+    print(f"At temperature {user_temperature}F, and wind speed {wind_speed} mph, the windchill is: {windchill:6.2f}F")
